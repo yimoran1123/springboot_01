3c37ae13e75e4b65ad634c0aa1e20d0ecc2396a7
yimoran1123 <72917862+yimoran1123@users.noreply.github.com>
2021-06-14 17:31:44 +0800

    Update UesrController.java

--- src/main/java/com/ymr/controller/UesrController.java
+++ src/main/java/com/ymr/controller/UesrController.java
@@ -40,7 +40,7 @@ public class UesrController {
 
     @RequestMapping("/master")
     public String master(){
-        return "免密登陆";
+        return "免密登陆_修改代码";
     }
 
   /*  public static void main(String[] args) {
